d82a836071475556590a45e9386bb9228c6b01f4
Miguel París Díaz <mparisdiaz@gmail.com>
2014-02-19 15:37:27 +0100

    Start and finish a WebRtcSession in MainActivity
    Change-Id: Ic7d885945adc5302d4039c7f16f5060687d9662e

--- src/main/java/com/kurento/apps/android/webrtc/example/MainActivity.java
+++ src/main/java/com/kurento/apps/android/webrtc/example/MainActivity.java
@@ -1,7 +1,23 @@
 package com.kurento.apps.android.webrtc.example;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+	private WebRtcSession session;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		session = new WebRtcSession(this);
+		session.start();
+	}
+
+	@Override
+	protected void onDestroy() {
+		session.finish();
+		super.onDestroy();
+	}
 
 }
